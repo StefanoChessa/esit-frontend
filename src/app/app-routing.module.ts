c2fc4fcf633282f854ca5157252d45a4490a3657
StefanoChessa <sr.chessa@tiscali.it>
2021-04-27 12:01:30 +0200

    Aggiunta componente meteo form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MeteoFormComponent} from './meteo-form/meteo-form.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MeteoFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
