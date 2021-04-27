44cda00248734cbfa77c3b4c5da87db1116d64c5
StefanoChessa <sr.chessa@tiscali.it>
2021-04-27 12:01:55 +0200

    Aggiunta componente meteo form

---
+++ src/app/meteo-form/meteo-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meteo-form',
+  templateUrl: './meteo-form.component.html',
+  styleUrls: ['./meteo-form.component.css']
+})
+export class MeteoFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
